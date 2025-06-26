d43e2f8353558ab62fa094dd5d65ff768a698a98
bon-nina <215746632+bon-nina@users.noreply.github.com>
2025-06-26 17:10:59 +0200

    Linked buttons from index.html to script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let number1;
 let number2;
 let operator;
+let displayContent;
+
+
+const display = document.querySelector(".display");
+const acBtn = document.querySelector("#ac-btn");
+const delBtn = document.querySelector("#del-btn");
+const Btn1 = document.querySelector("#one-btn");
+const Btn2 = document.querySelector("#two-btn");
+const Btn3 = document.querySelector("#three-btn");
+const Btn4 = document.querySelector("#four-btn");
+const Btn5 = document.querySelector("#five-btn");
+const Btn6 = document.querySelector("#six-btn");
+const Btn7 = document.querySelector("#seven-btn");
+const Btn8 = document.querySelector("#eight-btn");
+const Btn9 = document.querySelector("#nine-btn");
+const multiplyBtn = document.querySelector("#multiply-btn");
+const divideBtn = document.querySelector("#divide-btn");
+const addBtn = document.querySelector("#addbtn");
+const subtractBtn = document.querySelector("#subtract-btn");
+const resultBtn = document.querySelector("#result-btn");
+
+
 
 function operate (operator,num1, num2) {
    if (operator == "+") {
