b39dede22ea2b15a211099ac0243ce45972b3698
bon-nina <215746632+bon-nina@users.noreply.github.com>
2025-06-26 17:24:58 +0200

    Added Event Listener to all buttons

--- script.js
+++ script.js
@@ -5,22 +5,55 @@ let displayContent;
 
 
 const display = document.querySelector(".display");
+
+//making buttons functioning
 const acBtn = document.querySelector("#ac-btn");
+acBtn.addEventListener("click", () => clearAll());
+
 const delBtn = document.querySelector("#del-btn");
-const Btn1 = document.querySelector("#one-btn");
-const Btn2 = document.querySelector("#two-btn");
-const Btn3 = document.querySelector("#three-btn");
-const Btn4 = document.querySelector("#four-btn");
-const Btn5 = document.querySelector("#five-btn");
-const Btn6 = document.querySelector("#six-btn");
-const Btn7 = document.querySelector("#seven-btn");
-const Btn8 = document.querySelector("#eight-btn");
-const Btn9 = document.querySelector("#nine-btn");
+delBtn.addEventListener("click", () => deleteOne());
+
+const btn1 = document.querySelector("#one-btn");
+btn1.addEventListener("click", () => assignVariables(1));
+
+const btn2 = document.querySelector("#two-btn");
+btn2.addEventListener("click", () => assignVariables(2));
+
+const btn3 = document.querySelector("#three-btn");
+btn3.addEventListener("click", () => assignVariables(3));
+
+const btn4 = document.querySelector("#four-btn");
+btn4.addEventListener("click", () => assignVariables(4));
+
+const btn5 = document.querySelector("#five-btn");
+btn5.addEventListener("click", () => assignVariables(5));
+
+const btn6 = document.querySelector("#six-btn");
+btn6.addEventListener("click", () => assignVariables(6));
+
+const btn7 = document.querySelector("#seven-btn");
+btn7.addEventListener("click", () => assignVariables(7));
+
+const btn8 = document.querySelector("#eight-btn");
+btn8.addEventListener("click", () => assignVariables(8));
+
+const btn9 = document.querySelector("#nine-btn");
+btn9.addEventListener("click", () => assignVariables(9));
+
 const multiplyBtn = document.querySelector("#multiply-btn");
+multiplyBtn.addEventListener("click", () => assignVariables("*"));
+
 const divideBtn = document.querySelector("#divide-btn");
+divideBtn.addEventListener("click", () => assignVariables("/"));
+
 const addBtn = document.querySelector("#addbtn");
+addBtn.addEventListener("click", () => assignVariables("+"));
+
 const subtractBtn = document.querySelector("#subtract-btn");
+subtractBtn.addEventListener("click", () => assignVariables("-"));
+
 const resultBtn = document.querySelector("#result-btn");
+resultBtn.addEventListener("click", () => operate(operator,number1,number2));
 
 
 
@@ -62,4 +95,16 @@ const calculation = {
         },
 }
 
-console.log(operate("+",21,7));
+function assignVariables () {
+
+
+}
+
+function clearAll () {
+
+
+}
+
+function deleteOne() {
+
+}
